d6f57e8328d340d2f6ea7fa99ca5340d546cf8bc
vladislav <nicochan00@mail.ru>
2022-07-08 01:08:03 +0300

    ending podject

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const item = document.querySelector('.item');
+const boxes = document.querySelectorAll('.box');
+
+item.addEventListener('dragstart', dragStart);
+item.addEventListener('dragend', dragEnd);
+
+boxes.forEach(item => {
+   item.addEventListener('dragover', dragOver);
+   item.addEventListener('drop', dragDrop);
+   item.addEventListener('dragenter', dragEnter);
+   item.addEventListener('dragleave', dragLeave);
+});
+
+function dragStart(e) {
+   e.target.classList.add('hold');
+   setTimeout(() => {
+      e.target.classList.add('hide');
+   }, 0);
+}
+
+function dragEnd(e) {
+   e.target.classList.remove('hold', 'hide');
+
+}
+
+function dragOver(e) {
+   e.preventDefault();  
+}
+
+function dragDrop(e) {
+   e.target.classList.remove('hovered');
+   e.target.append(item);
+}
+
+function dragEnter(e) {
+   e.target.classList.add('hovered');
+}
+
+function dragLeave(e) {
+   e.target.classList.remove('hovered');
+}
